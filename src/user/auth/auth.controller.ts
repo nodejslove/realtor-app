e7230941910c8feac35eab567c0225d3569de453
hamidreza <h.r.akbarnezhad@gmail.com>
2023-10-16 02:55:00 +0330

    add signup route

--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { SignupDto } from '../dto/auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/signup')
+  signup(@Body() body: SignupDto) {
+    return this.authService.signup();
+  }
+}
--- src/user/dto/auth.dto.ts
+++ src/user/dto/auth.dto.ts
@@ -10,7 +10,9 @@ export class SignupDto {
   @IsNotEmpty()
   name: string;
 
-  @Matches(/^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/)
+  @Matches(/^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/, {
+    message: 'Phone must be a valid phone number',
+  })
   phone: string;
 
   @IsEmail()
